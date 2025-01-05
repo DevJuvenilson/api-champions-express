0441dd8d0df15fe1f91210ca37417308febe20f6
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2025-01-05 18:11:39 -0300

    Create function getPlayerById

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,5 +1,5 @@
 import * as PlayerRepository from "../repositories/players-repository";
-import { noContent, ok } from "../utils/http-helper";
+import * as StatusCode from "../utils/http-helper";
 
 
 export const getPlayerService = async() => {
@@ -7,10 +7,23 @@ export const getPlayerService = async() => {
     let response = null;
 
     if (data) {
-        response = await ok(data);
+        response = await StatusCode.ok(data);
     } else {
-        response = await noContent();
+        response = await StatusCode.noContent();
     }
     
     return response;
 }
+
+export const getPlayerByIdService = async (id: number) => {
+    const data = await PlayerRepository.findPlayerById(id);
+    let response = null;
+
+    if (data) {
+        response = StatusCode.ok(data);
+    } else {
+        response = StatusCode.noContent()
+    }
+
+    return response;
+};
